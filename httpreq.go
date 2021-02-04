81992cc078f6be2b01ff898f73d0d6b4c74edcb0
yu.he <gannicus_yu@163.com>
2021-02-04 21:07:04 +0800

    add demo

--- httpreq.go
+++ httpreq.go
@@ -12,7 +12,6 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -85,7 +84,7 @@ func genReqAction(fs FuncSet) func() {
 		}
 
 		startTime := time.Now()
-		response, err := fakeDo(request)
+		response, err := client.Do(request)
 		elapsed := time.Since(startTime)
 
 		if err != nil {
@@ -130,89 +129,10 @@ func genReqAction(fs FuncSet) func() {
 	return action
 }
 
-func genReqActionV2(fs FuncSet) boomer.Task {
-	variables := fs.RScript.genVariables()
-	initUrl := fs.getURL(variables.InitVariables)
-	initBody := fs.getBody(variables.InitVariables)
-	initHeaders := fs.getHeaders(variables.InitVariables)
-
-	action := func() {
-		url := fmt.Sprintf("%s%s", fs.RScript.Domain, initUrl)
-		request, err := http.NewRequest(fs.Method, url, bytes.NewBuffer([]byte(initBody)))
-		if err != nil {
-			log.Fatalf("%v\n", err)
-		}
-
-		for k, v := range initHeaders {
-			request.Header.Set(k, v)
-		}
-
-		if fs.RScript.Debug {
-			fmt.Println(formatRequest(request))
-		}
-
-		startTime := time.Now()
-		response, err := fakeDo(request)
-		elapsed := time.Since(startTime)
-
-		if err != nil {
-			if verbose {
-				log.Printf("%v\n", err)
-			}
-			boomer.RecordFailure("http", "error", 0.0, err.Error())
-		} else {
-
-			body, err := ioutil.ReadAll(response.Body)
-			if err != nil {
-				log.Printf("%v\n", err)
-			} else {
-				var res map[string]interface{}
-				_ = jsoniter.Unmarshal(body, &res)
-				res["http_status_code"] = response.StatusCode
-				merged := make(map[string]interface{})
-				for k, v := range res {
-					merged[k] = v
-				}
-				if fs.assertTrue(merged) {
-					//fmt.Println("assert true", elapsed.Nanoseconds()/int64(time.Millisecond))
-					boomer.RecordSuccess(fs.Key, strconv.Itoa(response.StatusCode),
-						elapsed.Nanoseconds()/int64(time.Millisecond), response.ContentLength)
-				}
-
-			}
-			if fs.RScript.Debug {
-				log.Printf("Status Code: %d\n", response.StatusCode)
-				log.Println(string(body))
-
-			} else {
-				io.Copy(ioutil.Discard, response.Body)
-			}
-
-			response.Body.Close()
-		}
-	}
-	task := boomer.Task{
-		Weight: fs.Probability,
-		Name:   fs.Key,
-		Fn:     action,
-	}
-	return task
-}
-
 func formatRequest(r *http.Request) string {
 	data, err := httputil.DumpRequest(r, true)
 	if err != nil {
 		log.Fatal("Error")
 	}
 	return string(data)
-}
-
-func fakeDo(request *http.Request) (*http.Response, error) {
-	resp := http.Response{
-		Body:          ioutil.NopCloser(strings.NewReader(`{"name": "yu.he", "age": "10", "data": {"timeout":"100"}}`)),
-		StatusCode:    200,
-		ContentLength: 300,
-	}
-
-	return &resp, nil
 }
